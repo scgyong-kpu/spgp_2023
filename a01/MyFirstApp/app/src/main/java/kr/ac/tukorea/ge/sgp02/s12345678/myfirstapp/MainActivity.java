b2362ac3a25bb513608c98f8b8294c58edfc7488
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-03-09 15:26:06 +0900

    버튼이 여러개라면 이런 분기를 해야할수도 있다.

--- a01/MyFirstApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/myfirstapp/MainActivity.java
+++ a01/MyFirstApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/myfirstapp/MainActivity.java
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.pushMeButton:
+                break;
+        }
         TextView tv = findViewById(R.id.snumTextView);
         tv.setText("PushMe button clicked");
     }
