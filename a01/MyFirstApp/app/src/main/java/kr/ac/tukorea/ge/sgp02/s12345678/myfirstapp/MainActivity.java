62eef0035d9223c9cae0564a08a5d48018d48980
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-03-09 15:02:08 +0900

    onCreate 에서 text 를 "Just loaded" 로 변경.

--- a01/MyFirstApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/myfirstapp/MainActivity.java
+++ a01/MyFirstApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/myfirstapp/MainActivity.java
@@ -3,6 +3,7 @@ package kr.ac.tukorea.ge.sgp02.s12345678.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.snumTextView);
+        tv.setText("Just loaded");
     }
 }
