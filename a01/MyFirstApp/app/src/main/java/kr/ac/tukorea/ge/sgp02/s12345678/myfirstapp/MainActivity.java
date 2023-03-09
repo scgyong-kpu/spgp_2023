3fb10c6c5deac21129f6f02bd265953e21ed2ed8
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-03-09 15:22:46 +0900

    Added button

--- a01/MyFirstApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/myfirstapp/MainActivity.java
+++ a01/MyFirstApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/myfirstapp/MainActivity.java
@@ -3,16 +3,24 @@ package kr.ac.tukorea.ge.sgp02.s12345678.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.pushMeButton);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
         TextView tv = findViewById(R.id.snumTextView);
-        tv.setText("Just loaded");
+        tv.setText("PushMe button clicked");
     }
 }
