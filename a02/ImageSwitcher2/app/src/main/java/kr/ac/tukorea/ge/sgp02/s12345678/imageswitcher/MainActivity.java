b93d46c70475f113e3c5fe2c845f2d73d61dcd84
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-03-14 14:15:34 +0900

    TAG 는 상수로 쓰는 것이 좋다.

--- a02/ImageSwitcher2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher/MainActivity.java
+++ a02/ImageSwitcher2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +17,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnPrev(View view) {
-        Log.d("MainActivity", "Prev pressed");
+        Log.d(TAG, "Prev pressed");
     }
 
     public void onBtnNext(View view) {
-        Log.d("MainActivity", "Next pressed");
+        Log.d(TAG, "Next pressed");
     }
 }
