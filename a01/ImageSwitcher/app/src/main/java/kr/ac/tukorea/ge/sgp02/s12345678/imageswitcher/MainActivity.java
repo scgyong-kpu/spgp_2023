16675ec15ca855f097073af1966423ebd6277618
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-03-14 17:12:18 +0900

    Prev/Next 버튼에 이미지 사용.

--- a01/ImageSwitcher/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher/MainActivity.java
+++ a01/ImageSwitcher/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private int page;
     private ImageView mainImageView;
     private TextView pageTextView;
-    private Button prevButton, nextButton;
+    private ImageButton prevButton, nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
