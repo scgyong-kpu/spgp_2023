a6f01e84a097285c4570ef05c7232495087c7dbd
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-03-14 16:07:14 +0900

    TAG 는 문자열로 직접 정의하지 않고 class.getSimpleName() 을 이용한다.

--- a01/ImageSwitcher/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher/MainActivity.java
+++ a01/ImageSwitcher/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
