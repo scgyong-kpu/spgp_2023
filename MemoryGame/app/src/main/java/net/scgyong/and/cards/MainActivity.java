33f0e5f4919f387d2c2773b30aa0013a6fc37aa4
Kiyong Kim <empty email>
2023-03-15 14:06:50 +0900

    onClick 을 추가하고 ID 를 출력해 본다.

--- MemoryGame/app/src/main/java/net/scgyong/and/cards/MainActivity.java
+++ MemoryGame/app/src/main/java/net/scgyong/and/cards/MainActivity.java
@@ -3,12 +3,21 @@ package net.scgyong.and.cards;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnCard(View view) {
+        // TAG 생성과정을 보여 줄 것. TAG 와 , 사이에서 Alt+Enter 를 눌러야 한다
+        Log.d(TAG, "Card ID = " + view.getId());
     }
 }
