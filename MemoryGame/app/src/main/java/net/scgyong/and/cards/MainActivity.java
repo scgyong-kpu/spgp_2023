2d527675a94fc1a77684c9f542af98a9d078ac8f
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-03-15 14:17:24 +0900

    ID 로 index 를 얻는 한 가지 방법

--- MemoryGame/app/src/main/java/net/scgyong/and/cards/MainActivity.java
+++ MemoryGame/app/src/main/java/net/scgyong/and/cards/MainActivity.java
@@ -9,7 +9,21 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
+    private static final int[] BUTTON_IDS = new int[] {
+            R.id.card_00,R.id.card_01,R.id.card_02,R.id.card_03,
+            R.id.card_10,R.id.card_11,R.id.card_12,R.id.card_13,
+            R.id.card_20,R.id.card_21,R.id.card_22,R.id.card_23,
+            R.id.card_30,R.id.card_31,R.id.card_32,R.id.card_33,
+    };
+    private static int getIndexWithId(int id) {
+        for (int i = 0; i < BUTTON_IDS.length; i++) {
+            if (BUTTON_IDS[i] == id) {
+                return i;
+            }
+        }
+        Log.e(TAG, "Cannot find the button with id: " + id);
+        return -1;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +33,8 @@ public class MainActivity extends AppCompatActivity {
     public void onBtnCard(View view) {
         // TAG 생성과정을 보여 줄 것. TAG 와 , 사이에서 Alt+Enter 를 눌러야 한다
         Log.d(TAG, "Card ID = " + view.getId());
+
+        int cardIndex = getIndexWithId(view.getId());
+        Log.i(TAG, "Card Index = " + cardIndex);
     }
 }
