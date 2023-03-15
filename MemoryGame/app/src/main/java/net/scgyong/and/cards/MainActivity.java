0f3643bd0a691983da2778619ebefe338aab966e
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-03-15 14:22:24 +0900

    HashMap 을 이용하여 찾아도 좋다.

--- MemoryGame/app/src/main/java/net/scgyong/and/cards/MainActivity.java
+++ MemoryGame/app/src/main/java/net/scgyong/and/cards/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -15,14 +17,20 @@ public class MainActivity extends AppCompatActivity {
             R.id.card_20,R.id.card_21,R.id.card_22,R.id.card_23,
             R.id.card_30,R.id.card_31,R.id.card_32,R.id.card_33,
     };
-    private static int getIndexWithId(int id) {
+    private static HashMap<Integer, Integer> idMap;
+    static { // static 블럭은 이 클래스가 최초 로드되는 시점에 한 번 실행된다
+        idMap = new HashMap<>();
         for (int i = 0; i < BUTTON_IDS.length; i++) {
-            if (BUTTON_IDS[i] == id) {
-                return i;
-            }
+            idMap.put(BUTTON_IDS[i], i);
         }
-        Log.e(TAG, "Cannot find the button with id: " + id);
-        return -1;
+    }
+    private static int getIndexWithId(int id) {
+        Integer index = idMap.get(id);
+        if (index == null) {
+            Log.e(TAG, "Cannot find the button with id: " + id);
+            return -1;
+        }
+        return index;
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
