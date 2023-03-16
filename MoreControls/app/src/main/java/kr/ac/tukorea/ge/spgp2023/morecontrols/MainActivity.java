94be9b34d63557b808c6b9c2362e5464ae1e86e6
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-03-17 00:15:29 +0900

    added EditText

--- MoreControls/app/src/main/java/kr/ac/tukorea/ge/spgp2023/morecontrols/MainActivity.java
+++ MoreControls/app/src/main/java/kr/ac/tukorea/ge/spgp2023/morecontrols/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox goodProgrammerCheckbox;
     private TextView outputTextView;
     private TextView reactionTextView;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +26,18 @@ public class MainActivity extends AppCompatActivity {
         goodProgrammerCheckbox = findViewById(R.id.goodProgrammerCheckbox);
         outputTextView = findViewById(R.id.outputTextView);
         reactionTextView = findViewById(R.id.reactionTextView);
+        nameEditText = findViewById(R.id.nameEditText);
     }
 
     public void onBtnDoIt(View view) {
         Log.d(TAG, "Do It");
-        int strResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_for_good : R.string.pay_for_not_good;
-        outputTextView.setText(strResId);
+        int fmtResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_for_good_fmt : R.string.pay_for_not_good_fmt;
+        String name = nameEditText.getText().toString();
+        if (name.trim().length() == 0) {
+            name = "NoName";
+        }
+        String text = getString(fmtResId, name);
+        outputTextView.setText(text);
     }
 
     public void onCheckboxGoodProgrammer(View view) {
