d956e436012a854b75c4c25291650bd3a9d70d0f
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-03-16 23:48:58 +0900

    Add Button & checkbox.isChecked(). adding string resource, padding vs margin,

--- MoreControls/app/src/main/java/kr/ac/tukorea/ge/spgp2023/morecontrols/MainActivity.java
+++ MoreControls/app/src/main/java/kr/ac/tukorea/ge/spgp2023/morecontrols/MainActivity.java
@@ -3,12 +3,30 @@ package kr.ac.tukorea.ge.spgp2023.morecontrols;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private CheckBox goodProgrammerCheckbox;
+    private TextView outputTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goodProgrammerCheckbox = findViewById(R.id.goodProgrammerCheckbox);
+        outputTextView = findViewById(R.id.outputTextView);
+    }
+
+    public void onBtnDoIt(View view) {
+        Log.d(TAG, "Do It");
+        int strResId = goodProgrammerCheckbox.isChecked() ? R.string.pay_for_good : R.string.pay_for_not_good;
+        outputTextView.setText(strResId);
     }
 }
