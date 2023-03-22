9bd855670f34e66f2bf05431460ef7d60bb4fbb6
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-03-22 16:32:45 +0900

    하나의 함수로 정리.

--- MoreControls/app/src/main/java/kr/ac/tukorea/ge/spgp2023/morecontrols/GameView.java
+++ MoreControls/app/src/main/java/kr/ac/tukorea/ge/spgp2023/morecontrols/GameView.java
@@ -82,19 +82,8 @@ public class GameView extends View {
 
     private void drawSmiley(Canvas canvas, float left, float top, float width, float height) {
         canvas.save();
-        setCanvasRect(canvas, left, top, width, height);
-        drawSmiley(canvas);
-        canvas.restore();
-        Log.i(TAG, "saveCount=" + canvas.getSaveCount());
-    }
-
-    private void setCanvasRect(Canvas canvas, float left, float top, float width, float height) {
-        Log.i(TAG, "setCanvasRect("+left+","+top+","+width+","+height+"), saveCount=" + canvas.getSaveCount());
         canvas.translate(left, top);
         canvas.scale(width / 100f, height / 100f);
-    }
-
-    private void drawSmiley(Canvas canvas) {
         canvas.drawOval(0, 0, 100, 100, facePaint);
         if (canvas.getSaveCount() <= 3) {
             drawSmiley(canvas, 23, 33, 14, 14);
@@ -103,5 +92,7 @@ public class GameView extends View {
         canvas.drawCircle(30, 40, 7, outlinePaint);
         canvas.drawCircle(70, 40, 7, outlinePaint);
         canvas.drawArc(20, 20, 80, 80, 30, 120, false, outlinePaint);
+        canvas.restore();
+        Log.i(TAG, "saveCount=" + canvas.getSaveCount());
     }
 }
