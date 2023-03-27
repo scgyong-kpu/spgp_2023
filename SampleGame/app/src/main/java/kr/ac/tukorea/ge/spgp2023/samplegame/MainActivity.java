c22689aa83d338516b1834cb529c704c13f66477
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-03-27 23:15:32 +0900

    -------- Project Skeleton: SampleGame --------

---
+++ SampleGame/app/src/main/java/kr/ac/tukorea/ge/spgp2023/samplegame/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.ge.spgp2023.samplegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
