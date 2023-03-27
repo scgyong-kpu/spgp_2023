bebc6675c3d23346f7f941a06777e2573a5e312c
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-03-28 01:12:58 +0900

    회전 적용. 주목할 것: save()/restore(), atan2() 를 쓰는 것, radian/degree 차이 확인,  90도를 더해 주는것.

--- SampleGame/app/src/main/java/kr/ac/tukorea/ge/spgp2023/samplegame/Fighter.java
+++ SampleGame/app/src/main/java/kr/ac/tukorea/ge/spgp2023/samplegame/Fighter.java
@@ -5,13 +5,17 @@ import android.graphics.Canvas;
 import android.graphics.RectF;
 
 public class Fighter {
+    private static final float RADIUS = 1.25f;
+
     private static Bitmap bitmap;
     private RectF dstRect = new RectF();
 
+    private float x, y;
+    private float angle;
     public Fighter() {
-        float cx = 5.0f, y = 12.0f;
-        float r = 1.25f;
-        dstRect.set(cx-r, y, cx+r, y+2*r);
+        x = 5.0f;
+        y = 13.25f;
+        dstRect.set(x-RADIUS, y, x+RADIUS, y+2*RADIUS);
     }
 
     public static void setBitmap(Bitmap bitmap) {
@@ -22,11 +26,19 @@ public class Fighter {
     }
 
     public void draw(Canvas canvas) {
+        canvas.save();
+        canvas.rotate(angle, x, y);
         canvas.drawBitmap(bitmap, null, dstRect, null);
+        canvas.restore();
     }
 
     public void setPosition(float x, float y) {
-        float r = 1.25f;
-        dstRect.set(x-r, y-r, x+r, y+r);
+        float dx = x - this.x;
+        float dy = y - this.y;
+        double radian = Math.atan2(dy, dx);
+        angle = (float) Math.toDegrees(radian) + 90;
+        dstRect.set(x-RADIUS, y-RADIUS, x+RADIUS, y+RADIUS);
+        this.x = x;
+        this.y = y;
     }
 }
