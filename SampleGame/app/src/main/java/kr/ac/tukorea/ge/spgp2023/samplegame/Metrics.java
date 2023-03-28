0c35d774f8430d8097c157588acc7697cae332d8
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-03-29 03:26:47 +0900

    Metrics 에 정한 크기 내에서 움직이도록 한다.

--- SampleGame/app/src/main/java/kr/ac/tukorea/ge/spgp2023/samplegame/Metrics.java
+++ SampleGame/app/src/main/java/kr/ac/tukorea/ge/spgp2023/samplegame/Metrics.java
@@ -5,4 +5,9 @@ public class Metrics {
     public static float game_width = 9.0f;
     public static float game_height = 16.0f;
     public static int x_offset = 0, y_offset = 0;
+
+    public static void setGameSize(float width, float height) {
+        game_width = width;
+        game_height = height;
+    }
 }
