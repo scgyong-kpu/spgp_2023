39737dd2da00d8fcbff72bda9ef63e5fc0d69663
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-06 08:23:42 +0900

    Fighter 를 위한 class 와 bitmap 을 추가한다

---
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Fighter.java
@@ -0,0 +1,10 @@
+package kr.ac.tukorea.ge.spgp2023.dragonflight.game;
+
+import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.Sprite;
+
+public class Fighter extends Sprite {
+    // Android Studio 가 추천하는 대로 생성자를 만들었을 경우
+    public Fighter(int bitmapResId, float cx, float cy, float width, float height) {
+        super(bitmapResId, cx, cy, width, height);
+    }
+}
