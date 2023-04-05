df059718097badb6486b6aca6b77543e58c41f03
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-06 08:32:40 +0900

    상수는 정의해서 사용하는 습관을 들인다

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Fighter.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Fighter.java
@@ -4,7 +4,11 @@ import kr.ac.tukorea.ge.spgp2023.dragonflight.R;
 import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.Sprite;
 
 public class Fighter extends Sprite {
+    private static final float FIGHTER_X = 4.5f;
+    private static final float FIGHTER_Y = 14.8f;
+    private static final float FIGHTER_SIZE = 1.75f;
+
     public Fighter() {
-        super(R.mipmap.fighter, 4.5f, 14.8f, 1.75f, 1.75f);
+        super(R.mipmap.fighter, FIGHTER_X, FIGHTER_Y, FIGHTER_SIZE, FIGHTER_SIZE);
     }
 }
