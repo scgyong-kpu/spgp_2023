cc01facf4771a009bc700c5e866563a5ce0820bd
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-06 08:04:05 +0900

    MainActivity 를 ./app/ Package 로 옮겨둔다

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/MainActivity.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/MainActivity.java
@@ -1,8 +1,10 @@
-package kr.ac.tukorea.ge.spgp2023.dragonflight;
+package kr.ac.tukorea.ge.spgp2023.dragonflight.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import kr.ac.tukorea.ge.spgp2023.dragonflight.R;
 
 public class MainActivity extends AppCompatActivity {
 
