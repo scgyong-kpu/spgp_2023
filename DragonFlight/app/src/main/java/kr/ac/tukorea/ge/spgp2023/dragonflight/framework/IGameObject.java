c318b44f3b15ffad6e549b8146987390682dea64
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-06 08:09:42 +0900

    framework folder 를 가져온다. Android Studio 끼리 package drag-drop 을 하면 다음 commit 내용까지 자동으로 처리해 준다.

---
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/framework/IGameObject.java
@@ -0,0 +1,8 @@
+package kr.ac.tukorea.ge.spgp2023.samplegame.framework;
+
+import android.graphics.Canvas;
+
+public interface IGameObject {
+    public void update();
+    public void draw(Canvas canvas);
+}
