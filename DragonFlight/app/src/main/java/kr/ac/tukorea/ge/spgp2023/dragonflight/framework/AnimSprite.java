79f30308a4b9c833a3867319a48aa75daef7ad8c
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-07 00:35:08 +0900

    srcRect 를 지정해야 한다

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/framework/AnimSprite.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/framework/AnimSprite.java
@@ -1,7 +1,19 @@
 package kr.ac.tukorea.ge.spgp2023.dragonflight.framework;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
 public class AnimSprite extends Sprite {
+    protected Rect srcRect = new Rect();
     public AnimSprite(int bitmapResId, float cx, float cy, float width, float height) {
         super(bitmapResId, cx, cy, width, height);
+//        int w = bitmap.getWidth();
+        int srcHeight = bitmap.getHeight();
+        srcRect.set(0, 0, srcHeight, srcHeight);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, srcRect, dstRect, null);
     }
 }
