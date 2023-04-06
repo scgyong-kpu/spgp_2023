e6c4c388464e8f74c9a0fc6f3bf3d22a6c6b0c54
Kiyong Kim <scgyong.isu@gmail.com>
2023-04-06 23:07:33 +0900

    Level 별 Image Resource 다른 것 사용하기

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Enemy.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Enemy.java
@@ -4,7 +4,12 @@ import kr.ac.tukorea.ge.spgp2023.dragonflight.R;
 import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.Sprite;
 
 public class Enemy extends Sprite {
+
+    private static final int[] resIds = {
+            R.mipmap.f_01_01, R.mipmap.f_02_01, R.mipmap.f_03_01, R.mipmap.f_04_01, R.mipmap.f_05_01,
+            R.mipmap.f_06_01, R.mipmap.f_07_01, R.mipmap.f_08_01, R.mipmap.f_09_01, R.mipmap.f_10_01,
+    };
     public Enemy(int index, int level) {
-        super(R.mipmap.f_01_01, 0.9f * (2 * index + 1), 3.0f, 1.5f, 1.5f);
+        super(resIds[level], 0.9f * (2 * index + 1), 3.0f, 1.8f, 1.8f);
     }
 }
