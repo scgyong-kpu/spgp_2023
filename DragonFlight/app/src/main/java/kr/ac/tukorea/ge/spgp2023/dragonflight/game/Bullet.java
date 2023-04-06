6ca3def97e52095c0dfe566a54e0a0541c251888
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-06 09:06:19 +0900

    package name fix

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Bullet.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Bullet.java
@@ -4,8 +4,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
-import kr.ac.tukorea.ge.spgp2023.samplegame.framework.BaseScene;
-import kr.ac.tukorea.ge.spgp2023.samplegame.framework.IGameObject;
+import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.BaseScene;
+import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.IGameObject;
 
 public class Bullet implements IGameObject {
     protected float x, y, dx, dy, ex, ey;
