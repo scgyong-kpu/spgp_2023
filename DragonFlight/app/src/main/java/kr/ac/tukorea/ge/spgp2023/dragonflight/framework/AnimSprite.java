d0e1c7f986695e64c919bed22b55ab2659d5d6d9
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-07 00:57:08 +0900

    AnimSprite 는 단순반복하는 이미지이므로 time 을 update 에서 꼼꼼히 누적하지 않아도 된다. draw 에서 생성시각과의 차이로 frameIndex 를 계산한다.

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/framework/AnimSprite.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/framework/AnimSprite.java
@@ -6,7 +6,8 @@ import android.graphics.Rect;
 public class AnimSprite extends Sprite {
     protected Rect srcRect = new Rect();
     protected int frameCount;
-    protected float time, fps;
+    protected float fps;
+    protected long createdOn;
     protected int frameWidth, frameHeight;
     public AnimSprite(int bitmapResId, float cx, float cy, float width, float height, float fps, int frameCount) {
         super(bitmapResId, cx, cy, width, height);
@@ -21,18 +22,15 @@ public class AnimSprite extends Sprite {
             this.frameCount = frameCount;
         }
         srcRect.set(0, 0, frameWidth, frameHeight);
-    }
-
-    @Override
-    public void update() {
-        super.update();
-        time += BaseScene.frameTime;
-        int frameIndex = Math.round(time * fps) % frameCount;
-        srcRect.set(frameIndex * frameWidth, 0, (frameIndex + 1) * frameWidth, frameHeight);
+        createdOn = System.currentTimeMillis();
     }
 
     @Override
     public void draw(Canvas canvas) {
+        long now = System.currentTimeMillis();
+        float time = (now - createdOn) / 1000.0f;
+        int frameIndex = Math.round(time * fps) % frameCount;
+        srcRect.set(frameIndex * frameWidth, 0, (frameIndex + 1) * frameWidth, frameHeight);
         canvas.drawBitmap(bitmap, srcRect, dstRect, null);
     }
 }
