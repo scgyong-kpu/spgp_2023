1db742275320cff2ec1fdc19d01e4d8f1b2d31e6
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-07 00:47:08 +0900

    초당 29.7 프레임 속도로 애니메이션 한다.

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/framework/AnimSprite.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/framework/AnimSprite.java
@@ -5,21 +5,29 @@ import android.graphics.Rect;
 
 public class AnimSprite extends Sprite {
     protected Rect srcRect = new Rect();
-    protected int frameIndex, frameCount;
+    protected int frameCount;
+    protected float time, fps;
     protected int frameWidth, frameHeight;
-    public AnimSprite(int bitmapResId, float cx, float cy, float width, float height) {
+    public AnimSprite(int bitmapResId, float cx, float cy, float width, float height, float fps, int frameCount) {
         super(bitmapResId, cx, cy, width, height);
+        this.fps = fps;
         int imageWidth = bitmap.getWidth();
         frameHeight = bitmap.getHeight();
-        frameWidth = frameHeight;
-        frameCount = imageWidth / frameHeight;
+        if (frameCount == 0) {
+            frameWidth = frameHeight;
+            this.frameCount = imageWidth / frameHeight;
+        } else {
+            frameWidth = imageWidth / frameCount;
+            this.frameCount = frameCount;
+        }
         srcRect.set(0, 0, frameWidth, frameHeight);
     }
 
     @Override
     public void update() {
         super.update();
-        frameIndex = (frameIndex + 1) % frameCount;
+        time += BaseScene.frameTime;
+        int frameIndex = Math.round(time * fps) % frameCount;
         srcRect.set(frameIndex * frameWidth, 0, (frameIndex + 1) * frameWidth, frameHeight);
     }
 
