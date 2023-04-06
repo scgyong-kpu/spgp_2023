f272bcd97ae567bfba38a00019d3ec5f9e58bcce
Kiyong Kim <scgyong.isu@gmail.com>
2023-04-06 23:10:57 +0900

    Enemy 는 SPEED 의 속도로 아래로 떨어진다

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Enemy.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/Enemy.java
@@ -1,6 +1,7 @@
 package kr.ac.tukorea.ge.spgp2023.dragonflight.game;
 
 import kr.ac.tukorea.ge.spgp2023.dragonflight.R;
+import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.BaseScene;
 import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.Sprite;
 
 public class Enemy extends Sprite {
@@ -9,7 +10,16 @@ public class Enemy extends Sprite {
             R.mipmap.f_01_01, R.mipmap.f_02_01, R.mipmap.f_03_01, R.mipmap.f_04_01, R.mipmap.f_05_01,
             R.mipmap.f_06_01, R.mipmap.f_07_01, R.mipmap.f_08_01, R.mipmap.f_09_01, R.mipmap.f_10_01,
     };
+    private static final float SPEED = 2.0f;
+
     public Enemy(int index, int level) {
         super(resIds[level], 0.9f * (2 * index + 1), 3.0f, 1.8f, 1.8f);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        y += SPEED * BaseScene.frameTime;
+        fixDstRect();
     }
 }
