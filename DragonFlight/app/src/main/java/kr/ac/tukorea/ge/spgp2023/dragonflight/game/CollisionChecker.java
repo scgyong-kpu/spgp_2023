234ed7dd7bd4b6cf0cf17aa54763330972371dde
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-07 02:32:51 +0900

    MainScene 에서 하던 충돌체크를 CollisionChecker 라는 이름의 controller 에서 하도록 옮김.

---
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/CollisionChecker.java
@@ -0,0 +1,44 @@
+package kr.ac.tukorea.ge.spgp2023.dragonflight.game;
+
+import android.graphics.Canvas;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.BaseScene;
+import kr.ac.tukorea.ge.spgp2023.dragonflight.framework.IGameObject;
+
+public class CollisionChecker implements IGameObject {
+    private static final String TAG = CollisionChecker.class.getSimpleName();
+
+    @Override
+    public void update() {
+        MainScene scene = (MainScene) BaseScene.getTopScene();
+        ArrayList<IGameObject> enemies = scene.getObjectsAt(MainScene.Layer.enemy);
+        ArrayList<IGameObject> bullets = scene.getObjectsAt(MainScene.Layer.bullet);
+        for (IGameObject o1 : enemies) {
+            if (!(o1 instanceof Enemy)) {
+                continue;
+            }
+            Enemy enemy = (Enemy) o1;
+//            boolean removed = false;
+            for (IGameObject o2 : bullets) {
+                if (!(o2 instanceof Bullet)) {
+                    continue;
+                }
+                Bullet bullet = (Bullet) o2;
+                if (CollisionHelper.collides(enemy, bullet)) {
+                    Log.d(TAG, "Collision !!");
+                    scene.remove(bullet); // is this recyclable?
+                    scene.remove(enemy); // is this recyclable?
+//                    removed = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+    }
+}
--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/MainScene.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/game/MainScene.java
@@ -15,48 +15,15 @@ public class MainScene extends BaseScene {
     private static final String TAG = MainScene.class.getSimpleName();
     private final Fighter fighter;
 
-    enum Layer {
+    public enum Layer {
         enemy, bullet, player, controller, COUNT
     }
     public MainScene() {
         initLayers(Layer.COUNT);
         fighter = new Fighter();
         add(Layer.player, fighter);
-
-//        AnimSprite animSprite = new AnimSprite(R.mipmap.enemy_01, 4.5f, 5.0f, 1.8f, 1.8f, 10, 0);
-//        add(animSprite);
         add(Layer.controller, new EnemyGenerator());
-    }
-
-    @Override
-    public void update(long elapsedNanos) {
-        super.update(elapsedNanos);
-        checkCollision();
-    }
-
-    private void checkCollision() {
-        ArrayList<IGameObject> enemies = this.getObjectsAt(Layer.enemy);
-        ArrayList<IGameObject> bullets = this.getObjectsAt(Layer.bullet);
-        for (IGameObject o1 : enemies) {
-            if (!(o1 instanceof Enemy)) {
-                continue;
-            }
-            Enemy enemy = (Enemy) o1;
-//            boolean removed = false;
-            for (IGameObject o2 : bullets) {
-                if (!(o2 instanceof Bullet)) {
-                    continue;
-                }
-                Bullet bullet = (Bullet) o2;
-                if (CollisionHelper.collides(enemy, bullet)) {
-                    Log.d(TAG, "Collision !!");
-                    remove(bullet); // is this recyclable?
-                    remove(enemy); // is this recyclable?
-//                    removed = true;
-                    break;
-                }
-            }
-        }
+        add(Layer.controller, new CollisionChecker());
     }
 
     public boolean onTouchEvent(MotionEvent event) {
