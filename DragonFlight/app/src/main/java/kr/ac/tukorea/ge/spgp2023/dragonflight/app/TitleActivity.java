e727dab5d8260987ac98359e8b19feaeabb14f29
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-12 08:42:16 +0900

    Animation Duration 을 30초로 변경하고 progress 를 translationX 에 적용

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/TitleActivity.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/TitleActivity.java
@@ -1,15 +1,13 @@
 package kr.ac.tukorea.ge.spgp2023.dragonflight.app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
-import kr.ac.tukorea.ge.spgp2023.dragonflight.R;
+import androidx.appcompat.app.AppCompatActivity;
+
 import kr.ac.tukorea.ge.spgp2023.dragonflight.databinding.ActivityTitleBinding;
 
 public class TitleActivity extends AppCompatActivity {
@@ -27,14 +25,16 @@ public class TitleActivity extends AppCompatActivity {
     }
     private void createAnimator() {
         animator = ValueAnimator.ofFloat(0.0f, 0.5f);
-        animator.setDuration(5000);
+        animator.setDuration(30000);
         animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.setInterpolator(new LinearInterpolator());
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
                 float progress = (Float)valueAnimator.getAnimatedValue();
-                Log.d(TAG, "Progress: " + progress);
+                //Log.d(TAG, "Progress: " + progress);
+                float tx = -1 * binding.treeImageView.getWidth() * progress;
+                binding.treeImageView.setTranslationX(tx);
             }
         });
     }
@@ -62,11 +62,11 @@ public class TitleActivity extends AppCompatActivity {
         startActivity(new Intent(this, MainActivity.class));
     }
 
-    private float progress;
-    public void onBtnProgress(View view) {
-        progress += 0.05f;
-
-        float tx = -1 * binding.treeImageView.getWidth() * progress;
-        binding.treeImageView.setTranslationX(tx);
-    }
+//    private float progress;
+//    public void onBtnProgress(View view) {
+//        progress += 0.05f;
+//
+//        float tx = -1 * binding.treeImageView.getWidth() * progress;
+//        binding.treeImageView.setTranslationX(tx);
+//    }
 }
