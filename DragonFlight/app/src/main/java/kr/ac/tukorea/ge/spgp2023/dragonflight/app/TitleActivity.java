bb0952c2d8f3cea5796bd028d9f2ffb16d28a950
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-12 08:14:53 +0900

    TitleActivity -> MainActivity 실행하는 버튼 추가

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/TitleActivity.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/TitleActivity.java
@@ -2,7 +2,9 @@ package kr.ac.tukorea.ge.spgp2023.dragonflight.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.ac.tukorea.ge.spgp2023.dragonflight.R;
 
@@ -12,5 +14,9 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    public void onBtnStart(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
