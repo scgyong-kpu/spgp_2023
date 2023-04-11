0de7cf8cd39c71774291d4a0b974b5d6c7757a9d
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-12 08:31:43 +0900

    Highscore 버튼을 누를 때마다 ImageView 의 translationX 값을 변경

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/TitleActivity.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/TitleActivity.java
@@ -7,16 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 
 import kr.ac.tukorea.ge.spgp2023.dragonflight.R;
+import kr.ac.tukorea.ge.spgp2023.dragonflight.databinding.ActivityTitleBinding;
 
 public class TitleActivity extends AppCompatActivity {
+
+    private ActivityTitleBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_title);
+        binding = ActivityTitleBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 
     public void onBtnStart(View view) {
         startActivity(new Intent(this, MainActivity.class));
+    }
+
+    private float progress;
+    public void onBtnProgress(View view) {
+        progress += 0.05f;
+
+        float tx = -1 * binding.treeImageView.getWidth() * progress;
+        binding.treeImageView.setTranslationX(tx);
     }
 }
