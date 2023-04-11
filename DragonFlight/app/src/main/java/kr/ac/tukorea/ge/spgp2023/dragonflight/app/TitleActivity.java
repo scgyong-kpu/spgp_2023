64f8eb6dc7740f838ba7ce7b6306c6529e2a36aa
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-12 08:07:35 +0900

    TitleActivity 추가 (RightClick->New->Activity->EmptyActivity)

---
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/dragonflight/app/TitleActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.ge.spgp2023.dragonflight.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
