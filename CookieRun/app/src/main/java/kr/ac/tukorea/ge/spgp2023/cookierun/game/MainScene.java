a0e62e373f629ab19ca50cf836c550fce3c43269
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-17 00:59:04 +0900

    VertScrollBackground class 가져오고 이미지 3장 붙임. 게임을 가로 모드로 설정.

--- CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/MainScene.java
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/MainScene.java
@@ -1,8 +1,19 @@
 package kr.ac.tukorea.ge.spgp2023.cookierun.game;
 
+import kr.ac.tukorea.ge.spgp2023.cookierun.R;
 import kr.ac.tukorea.ge.spgp2023.framework.scene.BaseScene;
+import kr.ac.tukorea.ge.spgp2023.framework.view.Metrics;
 
 public class MainScene extends BaseScene {
+    public enum Layer {
+        bg, COUNT
+    }
     public MainScene() {
+        Metrics.setGameSize(16.0f, 9.0f);
+        initLayers(Layer.COUNT);
+
+        add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_1, 1.0f));
+        add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_2, 2.0f));
+        add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_3, 3.0f));
     }
 }
