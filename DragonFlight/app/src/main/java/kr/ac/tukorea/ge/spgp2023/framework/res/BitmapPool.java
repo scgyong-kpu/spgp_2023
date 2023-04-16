0e9fa8be3ae8f1d42641d35383a67260b10ad461
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-16 23:35:23 +0900

    Bitmap 을 load 할 때 scaled 항목을 false 로 주어 원본크기 그대로 로드하도록 한다.

--- DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/framework/res/BitmapPool.java
+++ DragonFlight/app/src/main/java/kr/ac/tukorea/ge/spgp2023/framework/res/BitmapPool.java
@@ -12,11 +12,17 @@ import kr.ac.tukorea.ge.spgp2023.framework.view.GameView;
 public class BitmapPool {
     private static final String TAG = BitmapPool.class.getSimpleName();
     private static HashMap<Integer, Bitmap> bitmaps = new HashMap<>();
+    private static BitmapFactory.Options opts;
+
     public static Bitmap get(int mipmapResId) {
         Bitmap bitmap = bitmaps.get(mipmapResId);
         if (bitmap == null) {
             Resources res = GameView.res;
-            bitmap = BitmapFactory.decodeResource(res, mipmapResId);
+            if (opts == null) {
+                opts = new BitmapFactory.Options();
+                opts.inScaled = false;
+            }
+            bitmap = BitmapFactory.decodeResource(res, mipmapResId, opts);
             Log.d(TAG, "Bitmap ID " + mipmapResId + " : " + bitmap.getWidth() + "x" + bitmap.getHeight());
             bitmaps.put(mipmapResId, bitmap);
         }
