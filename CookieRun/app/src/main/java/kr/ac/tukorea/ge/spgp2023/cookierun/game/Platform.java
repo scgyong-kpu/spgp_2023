16d21418c64fa33f517d84764ff8d516314d857d
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-18 05:38:45 +0900

    Platform.Type 에 의해 결정되는 정보들은 Platform.Type 이 관리하도록 한다

--- CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/Platform.java
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/Platform.java
@@ -5,20 +5,23 @@ import kr.ac.tukorea.ge.spgp2023.framework.objects.Sprite;
 
 public class Platform extends Sprite {
     public enum Type {
-        T_10x2, T_2x2, T_3x1
+        T_10x2, T_2x2, T_3x1;
+        int resId() { return resIds[this.ordinal()]; }
+        int width() { return widths[this.ordinal()]; }
+        int height() { return heights[this.ordinal()]; }
+        static int[] resIds = {
+                R.mipmap.cookierun_platform_480x48,
+                R.mipmap.cookierun_platform_124x120,
+                R.mipmap.cookierun_platform_120x40,
+        };
+        static int[] widths = { 10, 2, 3 };
+        static int[] heights = { 2, 2, 1 };
     }
-    protected static int[] resIds = {
-            R.mipmap.cookierun_platform_480x48,
-            R.mipmap.cookierun_platform_124x120,
-            R.mipmap.cookierun_platform_120x40,
-    };
-    protected static int[] widths = { 10, 2, 3 };
-    protected static int[] heights = { 2, 2, 1 };
     public Platform(Type type, float left, float top) {
-        int ord = type.ordinal();
-        setBitmapResource(resIds[ord]);
-        width = widths[ord];
-        height = heights[ord];
+        setBitmapResource(type.resId());
+        width = type.width();
+        height = type.height();
+        // Platform 은 x,y 를 사용하지 않고 dstRect 만을 사용하도록 한다.
         dstRect.set(left, top, left + width, top + height);
     }
 }
