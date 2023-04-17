75b3766fe9f9f8e316a2bcb789ef1f624c6cb495
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-18 03:59:55 +0900

    한장짜리 Image 대신 4프레임짜리 AnimSprite 를 사용

--- CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/Player.java
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/Player.java
@@ -1,10 +1,10 @@
 package kr.ac.tukorea.ge.spgp2023.cookierun.game;
 
 import kr.ac.tukorea.ge.spgp2023.cookierun.R;
-import kr.ac.tukorea.ge.spgp2023.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2023.framework.objects.AnimSprite;
 
-public class Player extends Sprite {
+public class Player extends AnimSprite {
     public Player() {
-        super(R.mipmap.cookie, 2.0f, 7.0f, 2.0f, 2.0f);
+        super(R.mipmap.cookie_player_run, 2.0f, 7.0f, 2.0f, 2.0f, 8, 4);
     }
 }
