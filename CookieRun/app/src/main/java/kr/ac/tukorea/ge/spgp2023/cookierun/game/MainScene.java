526698583fd44092717229cc2519d0f1a2a5e10c
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-18 03:54:17 +0900

    Cookie Player 추가

--- CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/MainScene.java
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/MainScene.java
@@ -6,7 +6,7 @@ import kr.ac.tukorea.ge.spgp2023.framework.view.Metrics;
 
 public class MainScene extends BaseScene {
     public enum Layer {
-        bg, COUNT
+        bg, player, COUNT
     }
     public MainScene() {
         Metrics.setGameSize(16.0f, 9.0f);
@@ -15,5 +15,7 @@ public class MainScene extends BaseScene {
         add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_1, 1.0f));
         add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_2, 2.0f));
         add(Layer.bg, new VertScrollBackground(R.mipmap.cookie_run_bg_3, 3.0f));
+
+        add(Layer.player, new Player());
     }
 }
---
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/Player.java
@@ -0,0 +1,10 @@
+package kr.ac.tukorea.ge.spgp2023.cookierun.game;
+
+import kr.ac.tukorea.ge.spgp2023.cookierun.R;
+import kr.ac.tukorea.ge.spgp2023.framework.objects.Sprite;
+
+public class Player extends Sprite {
+    public Player() {
+        super(R.mipmap.cookie, 2.0f, 7.0f, 2.0f, 2.0f);
+    }
+}
