af0a98178197504d761c80d7849a1786bf513d8c
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-30 23:24:29 +0900

    3개의 버튼 이미지를 Sprite 를 사용하여 보여줌.

--- CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/MainScene.java
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/MainScene.java
@@ -5,6 +5,7 @@ import android.view.MotionEvent;
 import java.util.Random;
 
 import kr.ac.tukorea.ge.spgp2023.cookierun.R;
+import kr.ac.tukorea.ge.spgp2023.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2023.framework.scene.BaseScene;
 import kr.ac.tukorea.ge.spgp2023.framework.view.Metrics;
 
@@ -12,7 +13,7 @@ public class MainScene extends BaseScene {
     private final Player player;
 
     public enum Layer {
-        bg, platform, item, player, controller, COUNT
+        bg, platform, item, player, ui, controller, COUNT
     }
     public MainScene() {
         Metrics.setGameSize(16.0f, 9.0f);
@@ -25,6 +26,9 @@ public class MainScene extends BaseScene {
         player = new Player();
         add(Layer.player, player);
 
+        add(Layer.ui, new Sprite(R.mipmap.btn_slide_n, 1.5f, 8.0f, 2.0f, 0.75f ));
+        add(Layer.ui, new Sprite(R.mipmap.btn_jump_n, 14.5f, 7.7f, 2.0f, 0.75f ));
+        add(Layer.ui, new Sprite(R.mipmap.btn_fall_n, 14.5f, 8.5f, 2.0f, 0.75f ));
         add(Layer.controller, new MapLoader());
         add(Layer.controller, new CollisionChecker(player));
     }
