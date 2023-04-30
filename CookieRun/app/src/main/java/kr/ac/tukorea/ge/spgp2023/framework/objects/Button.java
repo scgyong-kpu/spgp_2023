00f2e6440621c5a63c44aef2de290d745e114daa
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-30 23:39:06 +0900

    Sprite 대신 Button 으로 만들고 Touch Event 전달

--- CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/framework/objects/Button.java
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/framework/objects/Button.java
@@ -1,12 +1,20 @@
 package kr.ac.tukorea.ge.spgp2023.framework.objects;
 
+import android.util.Log;
 import android.view.MotionEvent;
 
 import kr.ac.tukorea.ge.spgp2023.framework.interfaces.ITouchable;
 
-public class Button implements ITouchable {
+public class Button extends Sprite implements ITouchable {
+    private static final String TAG = Button.class.getSimpleName();
+
+    public Button(int bitmapResId, float cx, float cy, float width, float height) {
+        super(bitmapResId, cx, cy, width, height);
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent e) {
+        Log.d(TAG, "Button.onTouch(" + System.identityHashCode(this) + ", " + e.getAction() + ", " + e.getX() + ", " + e.getY());
         return false;
     }
 }
