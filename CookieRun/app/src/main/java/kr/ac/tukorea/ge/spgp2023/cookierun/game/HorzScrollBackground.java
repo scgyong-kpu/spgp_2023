050d250739a2b827e84d6940188de361349f5d04
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-04-30 22:58:57 +0900

    Copy from Vert -> Horz

---
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/HorzScrollBackground.java
@@ -0,0 +1,33 @@
+package kr.ac.tukorea.ge.spgp2023.cookierun.game;
+
+import android.graphics.Canvas;
+
+import kr.ac.tukorea.ge.spgp2023.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp2023.framework.scene.BaseScene;
+import kr.ac.tukorea.ge.spgp2023.framework.view.Metrics;
+
+public class HorzScrollBackground extends Sprite {
+    private final float speed;
+    private final float height;
+    private float scroll;
+    public HorzScrollBackground(int bitmapResId, float speed) {
+        super(bitmapResId, Metrics.game_width / 2, Metrics.game_height / 2, Metrics.game_width, Metrics.game_height);
+        this.height = bitmap.getHeight() * Metrics.game_width / bitmap.getWidth();
+        setSize(Metrics.game_width, height);
+        this.speed = speed;
+    }
+    @Override
+    public void update() {
+        scroll += speed * BaseScene.frameTime;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        float curr = scroll % height;
+        if (curr > 0) curr -= height;
+        while (curr < Metrics.game_height) {
+            dstRect.set(0, curr, Metrics.game_width, curr + height);
+            canvas.drawBitmap(bitmap, null, dstRect, null);
+            curr += height;
+        }
+    }}
