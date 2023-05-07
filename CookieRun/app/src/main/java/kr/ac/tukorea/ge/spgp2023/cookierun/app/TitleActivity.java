76ad55cdc8e230b9aae543d8fe9355f06bd755a9
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-07 10:03:52 +0900

    TitleActivity 생성

---
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/app/TitleActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.tukorea.ge.spgp2023.cookierun.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.ac.tukorea.ge.spgp2023.cookierun.R;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
