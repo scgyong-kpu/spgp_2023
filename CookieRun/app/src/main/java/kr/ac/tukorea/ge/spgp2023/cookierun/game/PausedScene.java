3bb3566cf157f06f628d52137bc9b8312cb46749
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-15 22:12:59 +0900

    반투명한 검정으로 배경 채움

--- CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/PausedScene.java
+++ CookieRun/app/src/main/java/kr/ac/tukorea/ge/spgp2023/cookierun/game/PausedScene.java
@@ -14,14 +14,24 @@ public class PausedScene extends BaseScene {
     }
     public PausedScene() {
         initLayers(Layer.COUNT);
+        add(Layer.bg, new Sprite(R.mipmap.trans_50b, 8.0f, 4.5f, 16, 9));
         add(Layer.bg, new Sprite(R.mipmap.bg_city_landscape, 8.0f, 4.5f, 12, 6.75f));
         title = new Sprite(R.mipmap.cookie_run_title, 8.0f, 4.5f, 3.69f, 1.36f);
         add(Layer.bg, title);
-        add(Layer.touch, new Button(R.mipmap.btn_resume_n, 14.5f, 1.0f, 2.0f, 0.75f, new Button.Callback() {
+        add(Layer.touch, new Button(R.mipmap.btn_resume_n, 8f, 3.5f, 2.667f, 1f, new Button.Callback() {
             @Override
             public boolean onTouch(Button.Action action) {
                 if (action == Button.Action.pressed) {
                     popScene();
+                }
+                return false;
+            }
+        }));
+        add(Layer.touch, new Button(R.mipmap.btn_exit_n, 8f, 5.5f, 2.667f, 1f, new Button.Callback() {
+            @Override
+            public boolean onTouch(Button.Action action) {
+                if (action == Button.Action.pressed) {
+                    finishActivity();
                 }
                 return false;
             }
