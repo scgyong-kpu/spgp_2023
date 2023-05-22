b62344fe6898541a51c8369ca470ddecd59ac6b2
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-22 21:36:07 +0900

    MainScene 생성 후 push.

--- TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/app/MainActivity.java
+++ TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/app/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 
 import kr.ac.tukorea.ge.spgp2023.framework.activity.BaseGameActivity;
 import kr.ac.tukorea.ge.spgp2023.tudefence.R;
+import kr.ac.tukorea.ge.spgp2023.tudefence.game.scene.MainScene;
 
 public class MainActivity extends BaseGameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        new MainScene().pushScene();
     }
 }
---
+++ TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/scene/MainScene.java
@@ -0,0 +1,6 @@
+package kr.ac.tukorea.ge.spgp2023.tudefence.game.scene;
+
+import kr.ac.tukorea.ge.spgp2023.framework.scene.BaseScene;
+
+public class MainScene extends BaseScene {
+}
