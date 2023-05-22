517daf5bc7e3830dea9c8e2a1b9be3a8650d68b5
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-23 00:33:56 +0900

    desert.tmj 를 이용하여 map 그림.

--- TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/scene/MainScene.java
+++ TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/scene/MainScene.java
@@ -3,20 +3,24 @@ package kr.ac.tukorea.ge.spgp2023.tudefence.game.scene;
 import kr.ac.tukorea.ge.spgp2023.framework.map.MapLoader;
 import kr.ac.tukorea.ge.spgp2023.framework.objects.TiledBackground;
 import kr.ac.tukorea.ge.spgp2023.framework.scene.BaseScene;
+import kr.ac.tukorea.ge.spgp2023.framework.view.Metrics;
 
 public class MainScene extends BaseScene {
     private enum Layer {
         bg, COUNT
     }
+
+    public MainScene() {
+        Metrics.setGameSize(16, 9);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
         initLayers(Layer.COUNT);
         add(Layer.bg,
-                new TiledBackground("map", "earth_horz.tmj")
-                        .setFitHeight()
-                        .setScroll(10, 0)
-                        .setWraps(true)
+                new TiledBackground("map", "desert.tmj")
+                        .setFitWidth()
         );
     }
 }
