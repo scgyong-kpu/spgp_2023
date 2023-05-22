48a92eca4b9045921d28eff48784413e1b796025
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-22 22:18:21 +0900

    Start to Read map using MapLoader

---
+++ TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/framework/map/MapLoader.java
@@ -0,0 +1,46 @@
+package kr.ac.tukorea.ge.spgp2023.framework.map;
+
+import android.content.res.AssetManager;
+import android.util.JsonReader;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import kr.ac.tukorea.ge.spgp2023.framework.view.GameView;
+
+public class MapLoader {
+    private static final String TAG = MapLoader.class.getSimpleName();
+    private final AssetManager assets;
+    private String folder;
+
+    public MapLoader() {
+        this.assets = GameView.view.getContext().getAssets();
+    }
+
+    public TiledMap loadAsset(String folder, String tmjFile) {
+        this.folder = folder;
+        try {
+            InputStream is = assets.open(folder + "/" + tmjFile);
+            JsonReader reader = new JsonReader(new InputStreamReader(is));
+            return readMap(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private TiledMap readMap(JsonReader reader) throws IOException {
+        Log.v(TAG, "Reading map:");
+        TiledMap map = new TiledMap();
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            Log.d(TAG, "Map JSON TMJ key = " + name);
+            reader.skipValue();
+        }
+
+        return null;
+    }
+}
--- TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/scene/MainScene.java
+++ TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/scene/MainScene.java
@@ -1,6 +1,12 @@
 package kr.ac.tukorea.ge.spgp2023.tudefence.game.scene;
 
+import kr.ac.tukorea.ge.spgp2023.framework.map.MapLoader;
 import kr.ac.tukorea.ge.spgp2023.framework.scene.BaseScene;
 
 public class MainScene extends BaseScene {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new MapLoader().loadAsset("map", "earth.tmj");
+    }
 }
