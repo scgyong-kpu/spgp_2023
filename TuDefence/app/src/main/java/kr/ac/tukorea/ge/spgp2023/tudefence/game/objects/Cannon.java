38cab3885b2b45d0a022c46a0b15c6b6a163432c
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-24 11:59:24 +0900

    barrel 추가

--- TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/objects/Cannon.java
+++ TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/objects/Cannon.java
@@ -1,6 +1,7 @@
 package kr.ac.tukorea.ge.spgp2023.tudefence.game.objects;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.RectF;
 
 import kr.ac.tukorea.ge.spgp2023.framework.objects.Sprite;
@@ -10,14 +11,25 @@ import kr.ac.tukorea.ge.spgp2023.tudefence.R;
 public class Cannon extends Sprite {
     private int level;
     private float power, interval;
+    private Bitmap barrelBitmap;
+    private RectF barrelRect;
     public Cannon(int level, int x, int y) {
         super(BITMAP_IDS[level - 1], x, y, 2, 2);
         this.level = level;
         this.power = level * 2;
         this.interval = 5.0f - level / 2.0f;
+        barrelBitmap = BitmapPool.get(R.mipmap.tank_barrel);
+        barrelRect = new RectF(dstRect);
+        barrelRect.inset(-1.5f, -1.5f);
     }
     private static int[] BITMAP_IDS = {
             R.mipmap.f_01_01,R.mipmap.f_02_01,R.mipmap.f_03_01,R.mipmap.f_04_01,R.mipmap.f_05_01,
             R.mipmap.f_06_01,R.mipmap.f_07_01,R.mipmap.f_08_01,R.mipmap.f_09_01,R.mipmap.f_10_01,
     };
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        canvas.drawBitmap(barrelBitmap, null, barrelRect, null);
+    }
 }
