b155462c5fd6e7513fd398207c1d7e003879e21e
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-29 17:29:12 +0900

    설치가능한곳에 빨간 사각형으로 표시.

--- TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/objects/Selector.java
+++ TuDefence/app/src/main/java/kr/ac/tukorea/ge/spgp2023/tudefence/game/objects/Selector.java
@@ -1,5 +1,7 @@
 package kr.ac.tukorea.ge.spgp2023.tudefence.game.objects;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -14,15 +16,22 @@ import kr.ac.tukorea.ge.spgp2023.tudefence.game.scene.MainScene;
 public class Selector extends Sprite {
     private static final String TAG = Selector.class.getSimpleName();
     private final TiledBackground bg;
+    private final Paint candPaint;
+    private float candidateX, candidateY;
 
     public Selector(TiledBackground tiledBg) {
         super(R.mipmap.selection, -1, -1, 2, 2);
         this.bg = tiledBg;
+
+        candPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        candPaint.setColor(0x7FBF3F3F);
+        candPaint.setStyle(Paint.Style.FILL);
     }
 
     public boolean onTouch(int action, float gx, float gy) {
         Cannon cannon = findCannonAt(gx, gy);
         if (cannon != null) {
+            candidateX = candidateY = -1;
             if (action == MotionEvent.ACTION_UP) {
                 cannon.upgrade();
             } else {
@@ -37,12 +46,33 @@ public class Selector extends Sprite {
         int x = Math.round(gx);
         int y = Math.round(gy);
         boolean canInstall = bg.canInstallAt(x, y);
-        if (!canInstall || action != MotionEvent.ACTION_UP) return true;
+        if (canInstall) {
+            if (action != MotionEvent.ACTION_UP) {
+                candidateX = x;
+                candidateY = y;
+                return true;
+            }
+            candidateX = candidateY = -1;
+        } else {
+            candidateX = candidateY = -1;
+            return true;
+        }
         cannon = new Cannon(1, x, y);
         BaseScene scene = BaseScene.getTopScene();
         scene.add(MainScene.Layer.cannon, cannon);
         moveTo(x, y);
         return true;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        if (candidateX >= 0) {
+            canvas.drawRect(
+                    candidateX - 1, candidateY - 1,
+                    candidateX + 1, candidateY + 1,
+                    candPaint);
+        }
     }
 
     private RectF instRect = new RectF();
