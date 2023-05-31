731a58f84edd6f3665716c39951a44cd775b50b6
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-06-01 08:27:32 +0900

    JSON 으로부터 Array 를 읽어들일 구조 잡기

--- TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/data/Song.java
+++ TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/data/Song.java
@@ -1,4 +1,34 @@
 package kr.ac.tukorea.ge.spgp2023.taptu.data;
 
+import android.content.Context;
+import android.util.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Song {
+    public static ArrayList<Song> load(Context context, String filename) {
+        ArrayList<Song> songs = new ArrayList<>();
+        try {
+            InputStream is = context.getAssets().open(filename);
+            JsonReader jr = new JsonReader(new InputStreamReader(is));
+            jr.beginArray();
+            while (jr.hasNext()) {
+                Song song = loadSong(jr);
+                if (song != null) {
+                    songs.add(song);
+                }
+            }
+            jr.endArray();
+            jr.close();
+        } catch (IOException e) {
+        }
+        return songs;
+    }
+
+    private static Song loadSong(JsonReader jr) {
+        return null;
+    }
 }
