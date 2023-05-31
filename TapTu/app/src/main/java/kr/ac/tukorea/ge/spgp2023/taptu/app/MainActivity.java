0c5ac7ca1601d788f35476b62425e5c28795ed87
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-05-31 16:18:50 +0900

    ViewBinding 지원

--- TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/app/MainActivity.java
+++ TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/app/MainActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kr.ac.tukorea.ge.spgp2023.taptu.R;
+import kr.ac.tukorea.ge.spgp2023.taptu.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
