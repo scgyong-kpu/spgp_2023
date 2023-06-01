8812a0615d3b9a54a80fc3fe44d9a6327dde76d6
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-06-01 09:03:37 +0900

    listView 에 Adapter 설정

--- TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/app/MainActivity.java
+++ TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/app/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 
 import java.util.ArrayList;
 
@@ -23,7 +26,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         songs = Song.loadSongs(this, "songs.json");
+
+        binding.listView.setAdapter(listAdapter);
     }
+
+    private BaseAdapter listAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    };
 
     public void onBtnStart(View view) {
     }
