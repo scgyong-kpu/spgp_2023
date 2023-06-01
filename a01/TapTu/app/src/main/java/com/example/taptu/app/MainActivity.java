2f4c32861f63d7ce277ffd26d5337685963cba97
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-06-01 15:43:12 +0900

    ListAdapter 구현

--- a01/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
+++ a01/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.taptu.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
 import com.example.taptu.R;
 import com.example.taptu.data.Song;
@@ -13,16 +18,46 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private ActivityMainBinding binding;
     private ArrayList<Song> songs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         songs = Song.loadSongs(this, "songs.json");
+        binding.listView.setAdapter(listAdapter);
     }
+    private ListAdapter listAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            Log.d(TAG, "getCount() is called");
+            return 100;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            Log.d(TAG, "getView(" + i + ") is called");
+            TextView tv = new TextView(MainActivity.this);
+            tv.setText("TextView #" + i);
+            return tv;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+    };
 
     public void onBtnStart(View view) {
     }
