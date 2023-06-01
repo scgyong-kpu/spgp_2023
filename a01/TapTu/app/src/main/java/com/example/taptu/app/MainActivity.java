123d5c03d98bf3379ad2bcb62b5c022850eb9e57
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-06-01 15:27:52 +0900

    ViewBinding 적용, songs 를 멤버변수로.

--- a01/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
+++ a01/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
@@ -7,17 +7,21 @@ import android.view.View;
 
 import com.example.taptu.R;
 import com.example.taptu.data.Song;
+import com.example.taptu.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private ArrayList<Song> songs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ArrayList<Song> songs = Song.loadSongs(this, "songs.json");
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        songs = Song.loadSongs(this, "songs.json");
     }
 
     public void onBtnStart(View view) {
