1bde98c8f04e6be23f7d2c7c16b6628d2ac06f77
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-06-01 15:12:17 +0900

    Song load 시도.

--- a01/TapTu/app/src/main/java/com/example/taptu/data/Song.java
+++ a01/TapTu/app/src/main/java/com/example/taptu/data/Song.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.AssetManager;
 import android.util.JsonReader;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -19,6 +20,7 @@ public class Song {
             jr.beginArray();
             while (jr.hasNext()) {
                 // read a song object
+                Song song = loadSong(jr);
             }
             jr.endArray();
             jr.close();
@@ -27,5 +29,15 @@ public class Song {
             //return songs;
         }
         return songs;
+    }
+
+    private static Song loadSong(JsonReader jr) throws IOException {
+        jr.beginObject();
+        while (jr.hasNext()) {
+            String name = jr.nextName();
+            jr.skipValue();
+        }
+        jr.endObject();
+        return null;
     }
 }
