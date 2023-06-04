a990d83124c4863c7c649d72ce0dcb9c983435e5
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-06-05 00:11:40 +0900

    new->Activity->Empty Activity->GameActivity

---
+++ TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/app/GameActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.ge.spgp2023.taptu.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
