e7df35d1849b0e1eb7fdd885ac11ca67faf5cc0f
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-06-05 01:01:00 +0900

    bg 추가

--- TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/game/PlayScene.java
+++ TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/game/PlayScene.java
@@ -1,6 +1,9 @@
 package kr.ac.tukorea.ge.spgp2023.taptu.game;
 
+import kr.ac.tukorea.ge.spgp2023.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spgp2023.framework.scene.BaseScene;
+import kr.ac.tukorea.ge.spgp2023.framework.view.Metrics;
+import kr.ac.tukorea.ge.spgp2023.taptu.R;
 import kr.ac.tukorea.ge.spgp2023.taptu.data.Song;
 
 public class PlayScene extends BaseScene {
@@ -10,8 +13,15 @@ public class PlayScene extends BaseScene {
         song = Song.get(index);
     }
 
+    public enum Layer {
+        bg, COUNT
+    }
+
     @Override
     protected void onStart() {
+        initLayers(Layer.COUNT);
+        float w = Metrics.game_width, h = Metrics.game_height;
+        add(Layer.bg, new Sprite(R.mipmap.bg, w/2, h/2, w, h));
         song.play();
     }
 
