3a1b2af4cc2105a8ff998bbf0e3ccaa1ac9a591f
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-06-05 15:25:55 +0900

    ListAdapter 사용

--- a02/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
+++ a02/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 
 import com.example.taptu.R;
 import com.example.taptu.data.Song;
@@ -21,7 +24,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         Song.loadSongs(this);
+
+        binding.listView.setAdapter(listAdapter);
     }
+    private ListAdapter listAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+    };
+
 
     public void onBtnStart(View view) {
     }
