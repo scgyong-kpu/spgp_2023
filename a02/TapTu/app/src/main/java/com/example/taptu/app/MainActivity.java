bef4ec842fb13be7205739485780413f1b97182d
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-06-05 14:38:48 +0900

    Activity package 옮기기, Portrait 만 지원하기, ActionBar 없애기

--- a02/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
+++ a02/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.taptu;
+package com.example.taptu.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.taptu.R;
 
 public class MainActivity extends AppCompatActivity {
 
