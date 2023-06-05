d9456481e7be55cb049b06b863b1760c15654c08
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-06-05 10:43:55 +0900

    Note 를 생성하는 NoteGen 클래스 만들고 한 프레임당 하나씩 생성.

--- TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/game/PlayScene.java
+++ TapTu/app/src/main/java/kr/ac/tukorea/ge/spgp2023/taptu/game/PlayScene.java
@@ -8,7 +8,7 @@ import kr.ac.tukorea.ge.spgp2023.taptu.R;
 import kr.ac.tukorea.ge.spgp2023.taptu.data.Song;
 
 public class PlayScene extends BaseScene {
-    private final Song song;
+    final Song song;
     private int timeMsec;
     private float speed;
 
@@ -19,7 +19,7 @@ public class PlayScene extends BaseScene {
     }
 
     public enum Layer {
-        bg, note, COUNT
+        bg, note, controller, COUNT
     }
 
     @Override
@@ -27,10 +27,7 @@ public class PlayScene extends BaseScene {
         initLayers(Layer.COUNT);
         float w = Metrics.game_width, h = Metrics.game_height;
         add(Layer.bg, new Sprite(R.mipmap.bg, w/2, h/2, w, h));
-        for (Song.Note note: song.notes) {
-            // temporary
-            add(Layer.note, NoteSprite.get(note));
-        }
+        add(Layer.controller, new NoteGen(this));
         song.play();
     }
 
