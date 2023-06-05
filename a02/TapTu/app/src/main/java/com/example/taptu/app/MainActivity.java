f0ac091b5d669f6cf3fdfe96324220ec0655c16f
Kim, Kiyong <scgyong@tukorea.ac.kr>
2023-06-05 14:45:37 +0900

    ViewBinding 지원

--- a02/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
+++ a02/TapTu/app/src/main/java/com/example/taptu/app/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.taptu.R;
+import com.example.taptu.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
